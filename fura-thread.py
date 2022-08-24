347007b85f33ae6986bddacdb76d236e80ee5b6b
Pep1t0 <daniel.requena@aol.com>
2022-08-24 19:03:36 +0200

    Pruebas diversas

--- fura-thread.py
+++ fura-thread.py
@@ -5,36 +5,43 @@ import os
 import time
 import concurrent.futures
 
-os.system('clear')
+# os.system('clear')
 
-FILE_TO_PROCESS = 'subdomains-top1million-5000.txt'
-DOMAIN_TO_PROCESS = 'thetoppers.htb'
+FILE_TO_PROCESS = 'subdomains.txt'
+DOMAIN_TO_PROCESS = '10.129.18.214'
 
 NUM_WORKERS = 3
 
 def check_subdomain(address, timeout=2):
     try:
         resp = requests.get(address, timeout=timeout)
+        
         resp.raise_for_status()
-        print('[EXISTE!!!!!!!!!!!] El dominio existe!!!',address, resp, '\n',sep='\t')
+    #    print('[EXISTE!!!!!!!!!!!] El dominio existe!!!',address, resp, '\n',sep='\t')
+    
     except requests.exceptions.HTTPError as err:
-        print('[HTTPError]', address)
-    except requests.exceptions.ConnectionError as err:
-        print('[URL Erronea] No existe',address, sep='\t')
-    except requests.exceptions.Timeout as err:
-        print('Excedido el tiempo de espera', address)
-    except requests.exceptions.RequestException as err:
-        print('Error general', err)
+        #if err.response.status_code == 404:
+        print("[+] Descubierto subdominio:",address)
+        
+    except requests.exceptions.ConnectionError:
+        # print('[URL Erronea] No existe',address, sep='\t')
+        pass
+    #except requests.exceptions.Timeout as err:
+    #    print('Excedido el tiempo de espera', address)
+    #except requests.exceptions.RequestException as err:
+    #    print('Error general', err)
+    else:
+        print("[+] Descubierto subdominio:",address)
  
 
 def file_fuzz (name_file, domain):
       
     try:
         s = open(name_file,'r')
-        fuzz_list = s.readlines()
-        fuzz_list = [line.rstrip('\n') for line in fuzz_list] # Eliminamos el retorno de carro \n que se añade en linux con readlines
-
-        website_list = [('http://' + fuzz + '.' + domain) for fuzz in fuzz_list]
+        content = s.read()
+        fuzz_list = content.splitlines()
+        
+        website_list = [ (f"http://{fuzz}.{domain}") for fuzz in fuzz_list]
         s.close()
 
         return website_list
